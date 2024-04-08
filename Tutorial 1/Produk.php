380241c31f34281b030cca1b62c2a319dbd4d7bc
YehezkielRumapea032 <148190973+YehezkielRumapea@users.noreply.github.com>
2024-04-08 14:47:22 +0700

    property

--- Tutorial 1/Produk.php
+++ Tutorial 1/Produk.php
@@ -3,12 +3,16 @@
 //Jualan komik & Game
 
 class produk{
-    public $judul;
-    public $penulis;
-    public $penerbit;
-    public $harga;
+    public $judul = 'judul';
+    public $penulis = 'penulis';
+    public $penerbit = 'penerbit';
+    public $harga = 0 ;
 }
 
 $produk1 = new produk();
-var_dump($produk1);
+$produk1-> judul ="Vagabond";
+$produk1-> penulis = "Takehiko Inoue";
+$produk1-> penerbit = "Shounen Jump";
+$produk1-> harga = "300.000";
+echo "Komik : $produk1->judul, $produk1->harga";
 $produk2 = new produk();

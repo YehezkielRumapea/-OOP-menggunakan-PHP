3a9176eb5b47dfe9da33f5a433811dbdb633e790
YehezkielRumapea032 <148190973+YehezkielRumapea@users.noreply.github.com>
2024-04-08 14:19:56 +0700

    property & Method

---
+++ Tutorial 1/Produk.php
@@ -0,0 +1,14 @@
+<?php
+
+//Jualan komik & Game
+
+class produk{
+    public $judul;
+    public $penulis;
+    public $penerbit;
+    public $harga;
+}
+
+$produk1 = new produk();
+var_dump($produk1);
+$produk2 = new produk();

1de4cef3cd1169348263c34e4131da67c561fbbc
13323032_Yehezkiel Rumapea <148190973+YehezkielRumapea@users.noreply.github.com>
2024-04-14 09:21:44 +0700

    Delete Tutorial 2 directory

--- Tutorial 2/app/views/about/index.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-<h1> About Me<h1>
-    <img src="<?=BASEURL;?>/img/th.jpeg" alt="The rock" class="rounded-circle shadow">
-        <p>Halo nama saya <?= $data['nama'];?> umur saya <?= $data['umur'];?> saya seorang <?= $data['pekerjaan'];?></p>
-        </div>
